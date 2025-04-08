d589d226a2461396275a7b0818f43d81cbca1311
Richard Anabeto Opoku <anabeto93@gmail.com>
2025-04-08 08:34:01 +0000

    switched to resource controller convention

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,5 @@ Route::middleware(['throttle:6,1'])->group(function() {
 
 // Admin routes (protected)
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/products', [AdminProductController::class, 'index'])->name('products.index');
-    Route::get('/products/create', [AdminProductController::class, 'create'])->name('products.create');
-    Route::post('/products', [AdminProductController::class, 'store'])->name('products.store');
-    Route::get('/products/{product}/edit', [AdminProductController::class, 'edit'])->name('products.edit');
-    Route::put('/products/{product}', [AdminProductController::class, 'update'])->name('products.update');
-    Route::delete('/products/{product}', [AdminProductController::class, 'destroy'])->name('products.destroy');
+    Route::resource('products', AdminProductController::class)->except(['show']);
 });
