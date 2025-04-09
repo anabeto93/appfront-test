76dc84d6ea8f6f0ab2c079ec91ca5405bdce301a
Richard Anabeto Opoku <anabeto93@gmail.com>
2025-04-09 05:44:31 +0000

    refactor: Remove direct env access and use service for exchange rate
    - Replace getExchangeRate method with ExchangeRateService in ProductController
    - Replace env() calls with config() in blade templates for URL access
    - Follow Laravel best practices by avoiding env() in application code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,38 +27,4 @@ class ProductController extends Controller
 
         return view('products.show', compact('product', 'exchangeRate'));
     }
-
-    /**
-     * @return float
-     */
-    private function getExchangeRate()
-    {
-        try {
-            $curl = curl_init();
-
-            curl_setopt_array($curl, [
-                CURLOPT_URL => "https://open.er-api.com/v6/latest/USD",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_TIMEOUT => 5,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-            ]);
-
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-
-            curl_close($curl);
-
-            if (!$err) {
-                $data = json_decode($response, true);
-                if (isset($data['rates']['EUR'])) {
-                    return $data['rates']['EUR'];
-                }
-            }
-        } catch (\Exception $e) {
-
-        }
-
-        return env('EXCHANGE_RATE', 0.85);
-    }
 }
